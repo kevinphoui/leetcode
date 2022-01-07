972fa1af8ca15ab3f23cbecb5e294253d9348441
Kevin Phouisangiem <70083807+kevinphoui@users.noreply.github.com>
2022-01-07 13:43:58 -0600

    Time: 34 ms (5.96%), Space: 40.6 MB (5.45%) - LeetHub

---
+++ 1507-reformat-date/1507-reformat-date.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String reformatDate(String date) {
+        //String x = "";
+        String[] month = {"x", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+        
+    
+        
+        // splits date
+        String[] input = date.split(" ");
+        System.out.println(Arrays.toString(input));
+        
+        //day
+        input[0] = input[0].replaceAll("[^0-9]", ""); // day
+        input[0] = String.format("%02d" , Integer.parseInt(input[0]));
+        
+        
+        // month
+        input[1] = ""+java.util.Arrays.asList(month).indexOf(input[1]);
+        input[1] = String.format("%02d" , Integer.parseInt(input[1]));
+
+                        // year        month        day
+        String output = input[2]+"-"+input[1]+"-"+input[0];
+        return output;
+    }
+}
