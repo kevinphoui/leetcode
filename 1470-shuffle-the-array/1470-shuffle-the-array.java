6d5b9b863e04844c7545951d5263288dfc8bb26c
Kevin Phouisangiem <70083807+kevinphoui@users.noreply.github.com>
2022-01-08 23:54:58 -0600

    Time: 0 ms (100.00%), Space: 38.7 MB (96.13%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.java
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -2,10 +2,7 @@ class Solution {
     public int[] shuffle(int[] nums, int n) {
         int[] arr = new int[nums.length];
         arr[0] = nums[0];
-        // split x and y
-        // then add x and y
-        
-        // or 
+
         // for first half, place in correct spot
         // x
         for (int i = 1; i < nums.length/2; i++){
