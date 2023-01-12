3f817f45272ab5559d9224fe4009cf28c7a69de1
Kevin Phouisangiem <70083807+kevinphoui@users.noreply.github.com>
2023-01-11 20:39:02 -0600

    Time: 1 ms (100.00%), Space: 43.1 MB (48.63%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,16 @@
+class Solution {
+    // approach: greedy
+    public boolean canJump(int[] nums) {
+        int lastPos = nums.length -1;
+        
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (i + nums[i] >= lastPos) {
+                lastPos = i;
+            }
+        }
+        
+        
+        
+        return lastPos == 0;
+    }
+}
